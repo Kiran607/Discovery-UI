fa3c1dcdd4e4e266128a1a308a22d506bc97a3a7
Kiran607 <ickirankiru@gmail.com>
2020-08-08 16:31:39 +0530

    [Kiran]Refactored map pargraph data from json file

--- src/components/LeftImage.jsx
+++ src/components/LeftImage.jsx
@@ -1,63 +1,37 @@
 import React from "react";
 import data from "../assests/data.json";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
 
 function LeftImage() {
   return (
-    <div className="image-text-row">
-      <div className="image-text-inner">
-        
-        <div className="displayImage">
-            <Card className="img-txt-img">
-                <CardMedia
-            className="card-image"
-            image={require("../assests/Golf_ComfortStation.jpg")}
-          />
-        </Card>
-        <div className="c-img-txt__gradient">
-            <div className="o-gradient-img__inner">
-
+    <div className="image-text-left">
+      <div className="image-text-row-left">
+        <div className="image-text-inner-left">
+          <figure className="displayImage-left">
+            <div className="img-txt-img">
+              <img
+                className="card-image"
+                src={require("../assests/Golf_ComfortStation.jpg")}
+              />
             </div>
-            </div> 
-        </div>
-        <div className="img-content">
-          <h3 id="img-txt-title">
-          Extraordinary finishing touches
-          </h3>
-          
-           <p>
-           Our signature clubhouses and comfort stations make 
-            every round of golf so much more than just a game –  
-            giving you the opportunity to enjoy outstanding 
-            amenities with every detail thought of and every taste 
-            catered to.
-           </p>
-          
-            <p>
-            Take a well-earned breather and recharge in our 
-            beautifully relaxed and inviting comfort stations. Admire 
-            some of nature’s most breathtaking views while kicking 
-            back with a refreshing drink and hearty snack during a 
-            round. And if you’ve worked up an appetite for 
-            something a little more substantial, you’ll also find 
-            refined gourmet offerings from our on course chefs 
-            ready and waiting to take your order. 
-            </p>
-
-           <p>
-           Pick up some professional tips with a private lesson or 
-           practice your game in our state-of-the-art facilities. 
-           </p>
-          
-          
-            <div>So you can choose to take on your fellow members in </div>
-            <div>ultra-competitive annual tournaments, or just play for </div>
-            <div>
-              the fun of it…barefoot with the family. This is luxury and{" "}
+            <div className="img-txt-left-bg">
+              <div className="img-txt-inner-left-gradient">
+                <img src={require("../assests/Golf2_bg.jpg")} id="bg-img" />
+              </div>
             </div>
-            <div>fun on your own terms.</div>
-          
+          </figure>
+          {data.leftText.map((postData, index) => {
+            return (
+              <div className="img-content-left" key={index}>
+                <h3 id="img-txt-title">{postData.textTitle}</h3>
+
+                <p>{postData.para1}</p>
+
+                <p>{postData.paragraph2}</p>
+
+                <p>{postData.paragraph3}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
